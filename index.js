7e5c216be7b3549de4ec75541d19c885ea3c291c
Prerak Mathur <mathur.prerak@gmail.com>
2021-09-29 05:03:10 +0530

    new req

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const courses = [
-    {
-        id : 1,
-        name: 'course1'
-    },
-    {
-        id : 2,
-        name: 'course2'
-    },
-    {
-        id : 3,
-        name: 'course3'
-    }
+  {
+    id: 1,
+    name: "course1",
+  },
+  {
+    id: 2,
+    name: "course2",
+  },
+  {
+    id: 3,
+    name: "course3",
+  },
 ];
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
 });
 
-app.get('/api/courses' , (req, res) => {
-  res.send([1, 2, 3]);
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-app.get('/api/courses/:id' , (req, res) => {
-    res.send([req.query]);
-  });
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res
+      .status(404)
+      .send(
+        "<h1 style='background-color:powderblue;'>Course with given ID was not found.</h1>"
+      );
 
-
-
+  res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => { console.log(`Server running on port ${port} 🔥`)}); 
+app.listen(port, () => {
+  console.log(`Server running on port ${port} 🔥`);
+});
